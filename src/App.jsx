2b837d6a8c32f19a4e386d3567de5df51cd84c2e
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-21 16:08:58 +0530

    prop drilling without context api

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ const App = () => {
 
   const [count,setCount] = useState(0);
 
+  /**
+   * If want to call buttons in count then we need to pass
+   * setCount also as a prop {count do not really use it 
+   * just passed down to button }  => becomes slightly 
+   * unmanageable to pass down the props .
+   * 
+   * propdrilling => drilling down the prop .
+  */
   return (
     <div>
-      <Count count={count}/>
-      <Buttons count={count} setCount={setCount}/>
+      
+      <Count count={count} setCount={setCount}/>
+
     </div>
   )
 }
 
-function Count({count}){
+function Count({count,setCount}){
   return <div>
     <b>Count is </b>{count}
+    <div>
+    <Buttons count={count} setCount={setCount}/>
+    </div>
+
   </div>
 }
 
