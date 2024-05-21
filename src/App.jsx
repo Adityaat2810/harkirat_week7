e969d4c20c7dee14063cabd71e85f00d65b8c4fd
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-21 13:50:26 +0530

    useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,57 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Dashboard} from './components/Dashboard';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { Dashboard } from './components/Dashboard';
 import { Landing } from './components/Landing';
 
 const App = () => {
+
+  // right way to navigate without hard reloadouter only
+  // const navigate=useNavigate();
+
   return (
-   <div> 
-    {/**this topbar remain consistent content below it changes */}
-    <div style={{background:"black",color:"white"}}>
-      Hi this is the topbar
-    </div>
-
     <div>
-      <button onClick={()=>{
-        window.location.href='/dashboard'
-      }}>Dashboard</button>
 
-      
-      <button onClick={()=>[
-        window.location.href='/'
-
-      ]}>Landing</button>
-    </div>
-
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AppBar />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
 
 
     </div>
   );
+}
+
+function AppBar() {
+
+
+  const navigate = useNavigate();
+
+  {/**this topbar remain consistent content below it changes */ }
+  return <>
+    <div style={{ background: "black", color: "white" }}>
+      Hi this is the topbar
+    </div>
+
+    <div>
+      <button onClick={() => {
+        navigate('/dashboard')
+      }}>Dashboard</button>
+
+
+      <button onClick={() => {
+        navigate('/')
+
+      }}>Landing</button>
+    </div>
+
+  </>
+
+
+
+
 }
 
 export default App;
