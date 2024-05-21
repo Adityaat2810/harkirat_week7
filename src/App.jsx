a801283d8f0033dbf8c16f913a8d9bdf120f1bed
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-21 15:47:57 +0530

    simple counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,39 @@
-import React,{Suspense, lazy} from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-const  Dashboard =  lazy(()=>import('./components/Dashboard'))
-const  Landing =  lazy(()=>import('./components/Landing'));
+import React, { useState } from 'react'
 
 const App = () => {
 
-  // right way to navigate without hard reloadouter only
-  //whatever use useNavigate need to be inside the browser router
-  // const navigate=useNavigate();
-
-
-  /**
-   * suspense api , asynchronous component fectching ,asynchronous 
-   * data fetching 
-   * */
-
+  const [count,setCount] = useState(0);
 
   return (
     <div>
-
-      <BrowserRouter>
-        <AppBar />
-        <Routes>
-          <Route path="/dashboard" element={<Suspense fallback={"...loading"}><Dashboard /></Suspense>} />
-          <Route path="/" element={<Suspense fallback={"...loading"}><Landing /></Suspense>}  />
-        </Routes>
-      </BrowserRouter>
-
-
+      <Count count={count}/>
+      <Buttons count={count} setCount={setCount}/>
     </div>
-  );
+  )
 }
 
-function AppBar() {
+function Count({count}){
+  return <div>
+    <b>Count is </b>{count}
+  </div>
+}
 
+function Buttons({count,setCount}){
+  return<>
+    <button onClick={()=>{
+      setCount(count+1)
 
-  const navigate = useNavigate();
+    }}> 
+      Increase count
+    </button>
 
-  {/**this topbar remain consistent content below it changes */ }
-  return <>
-    <div style={{ background: "black", color: "white" }}>
-      Hi this is the topbar
-    </div>
-
-    <div>
-      <button onClick={() => {
-        navigate('/dashboard')
-      }}>Dashboard</button>
-
-
-      <button onClick={() => {
-        navigate('/')
-
-      }}>Landing</button>
-    </div>
-
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>
+      Decrease count
+    </button>
   </>
 
-
-
-
 }
 
-export default App;
+export default App
