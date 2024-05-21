b7430f7357308791cf43c922da355b8d3978ce93
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-21 10:02:49 +0530

    learn basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Dashboard} from './components/Dashboard';
+import { Landing } from './components/Landing';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+   <div> 
+    {/**this topbar remain consistent content below it changes */}
+    <div style={{background:"black",color:"white"}}>
+      Hi this is the topbar
+    </div>
+
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+
+
+    </div>
+  );
 }
 
-export default App
+export default App;
